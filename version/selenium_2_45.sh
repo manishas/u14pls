f7843d2a948e865dd445bbab1fb6ad87cc50aa1b
Khoa Nguyen <vankhoa011@gmail.com>
2015-10-22 23:46:11 +0700

    Update dependency for google chrome stable

--- version/selenium_2_45.sh
+++ version/selenium_2_45.sh
@@ -13,10 +13,22 @@ sudo apt-get install -y xvfb firefox unzip
 sudo apt-get update && apt-get install -y \
   ca-certificates \
   gconf-service \
+  hicolor-icon-theme \
   libappindicator1 \
+  libasound2 \
+  libcanberra-gtk-module \
+  libcurl3 \
+  libexif-dev \
   libgconf-2-4 \
-  fonts-liberation\
+  libgl1-mesa-dri \
+  libgl1-mesa-glx \
+  libnspr4 \
+  libnss3 \
   libpango1.0-0 \
+  libv4l-0 \
+  libxss1 \
+  libxtst6 \
+  fonts-liberation\
   xdg-utils \
   --no-install-recommends
 
