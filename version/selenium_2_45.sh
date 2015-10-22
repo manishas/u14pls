e855ebd1ad933b75d59ec102af1f8b63c9fbd415
Khoa Nguyen <vankhoa011@gmail.com>
2015-10-22 23:29:22 +0700

    install dependency for chrome stable

--- version/selenium_2_45.sh
+++ version/selenium_2_45.sh
@@ -10,6 +10,27 @@ fi
 # Install prerequisites
 sudo apt-get install -y xvfb firefox unzip
 
+sudo apt-get update && apt-get install -y \
+  ca-certificates \
+  gconf-service \
+  hicolor-icon-theme \
+  libappindicator1 \
+  libasound2 \
+  libcanberra-gtk-module \
+  libcurl3 \
+  libexif-dev \
+  libgconf-2-4 \
+  libgl1-mesa-dri \
+  libgl1-mesa-glx \
+  libnspr4 \
+  libnss3 \
+  libpango1.0-0 \
+  libv4l-0 \
+  libxss1 \
+  libxtst6 \
+  xdg-utils \
+  --no-install-recommends
+
 # Install chrome
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
